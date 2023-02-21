f5d520aee3b452c828b7fd4150d34b1e2e302704
shaik Asif <smd.20sa@gmail.com>
2023-02-21 16:18:33 +0530

    dynamic placeholder

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,10 +1,19 @@
+import "./ExpenseItems.css";
+
 function ExpenseItem() {
+  const Expensedate = new Date(2023, 2, 14);
+  const Expensetitle = "Car Insurance";
+  const Expenseprice = 269;
+  const ExpenseLocation = "Bangalore";
   return (
-    <div>
-      <h2>Expense Item !!</h2>
-      <div>Feb 21</div>
-      <div>pizza</div>
-      <div>RS.350 /-</div>
+    <div className="expense-item">
+      <div>{Expensedate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">{Expensetitle}</h2>
+        <h2 className="expense-item h2">{ExpenseLocation}</h2>
+
+        <div className="expense-item__price">${Expenseprice}</div>
+      </div>
     </div>
   );
 }
