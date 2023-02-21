ca182f0a0737209e6e46c18ae5471c43ab21c172
shaik Asif <smd.20sa@gmail.com>
2023-02-21 13:58:18 +0530

    first task

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-  return <h2>Expense Item !!</h2>;
+  return (
+    <div>
+      <h2>Expense Item !!</h2>
+      <div>Feb 21</div>
+      <div>pizza</div>
+      <div>RS.350 /-</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
