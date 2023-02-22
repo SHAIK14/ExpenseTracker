4c631ae63d832481e92e202b4586a95884e1c4b2
shaik Asif <smd.20sa@gmail.com>
2023-02-22 13:07:11 +0530

    statehooks

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/card";
 import "./ExpenseItems.css";
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => console.log("clikced!");
+  const [title, settitle] = useState(props.title);
+  const clickHandler = () => {
+    settitle("updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2 className="expense-item h2">{props.title}</h2>
+        <h2 className="expense-item h2">{title}</h2>
 
         <div className="expense-item__price">${props.amount}</div>
       </div>
