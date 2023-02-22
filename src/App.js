c1856708e9d4182455964b1ac7e5d2902ecd1762
shaik Asif <smd.20sa@gmail.com>
2023-02-22 16:17:54 +0530

    forma and events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expenses = [
     {
@@ -25,7 +25,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
