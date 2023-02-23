571847735a36757fa3e92215c577566b0b2363ac
shaik Asif <smd.20sa@gmail.com>
2023-02-23 12:18:47 +0530

    showing outputs of selected year

--- src/components/Expenses/expenses.js
+++ src/components/Expenses/expenses.js
@@ -9,6 +9,9 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedyear) => {
     setfilteredYear(selectedyear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <div>
       <Card className="expenses">
@@ -16,26 +19,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           filterselect={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItem>
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
